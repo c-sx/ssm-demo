c1da9076bd50f8d00ace056984cfc8e0233da950
csx <csx9917@gmail.com>
2019-10-19 23:27:25 +0800

    ssm框架学习project

---
+++ src/main/java/com/mpocket/demo/service/DepartmentService.java
@@ -0,0 +1,22 @@
+package com.mpocket.demo.service;
+
+import com.mpocket.demo.bean.Department;
+import com.mpocket.demo.dao.DepartmentMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartmentService {
+	
+	@Autowired
+	private DepartmentMapper departmentMapper;
+
+	public List<Department> getDepts() {
+		// TODO Auto-generated method stub
+		List<Department> list = departmentMapper.selectByExample(null);
+		return list;
+	}
+
+}
